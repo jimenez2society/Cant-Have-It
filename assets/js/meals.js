5afdcd8f5ccb103a472fd3998cbdd9fa075d7030
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-04-13 12:32:26 -0600

    created meals page and restrictions page files. Set up intial api call

---
+++ assets/js/meals.js
@@ -0,0 +1,27 @@
+import { mealsCard } from "../../helpers/mealsCard.js";
+import { SPOONACULAR_API_KEY } from "../../util/keys.js";
+let data = JSON.parse(localStorage.getItem("pendingRequest"));
+
+// let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOONACULAR_API_KEY}&query=${data}&number=100`;
+
+//  if dietary restrictions
+
+// https://api.spoonacular.com/recipes/complexSearch?apiKey=6011a6a178b345a191a3bac0d7aa7aa3&diet=vegan
+
+//  if dietary restrictions and excludedItems
+
+// https://api.spoonacular.com/recipes/complexSearch?apiKey=6011a6a178b345a191a3bac0d7aa7aa3&query=peanuts&excludeIngredients=lime,salt&diet=vegan
+
+// else
+// https://api.spoonacular.com/recipes/complexSearch?apiKey=6011a6a178b345a191a3bac0d7aa7aa3&query=peanuts
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // handle data that's returned from the API call
+//     console.log(data.results);
+//   })
+//   //handle errors
+//   .catch((error) => {
+//     console.error(error);
+//   });

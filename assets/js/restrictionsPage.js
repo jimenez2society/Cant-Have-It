f94eddfe39abed99a52ec749b1e86d481a7d1ff3
rocas3096 <ozavala3096@gmail.com>
2023-04-15 17:06:55 -0500

    Added some premliminery javascript code for restrictions page

--- assets/js/restrictionsPage.js
+++ assets/js/restrictionsPage.js
@@ -1,4 +1,23 @@
 // get user restrictions info
+//sets the ingredientsFormRestrictionsPage variable to a form from the restirctions page html
+let ingredientsFormRestrictionsPage = document.querySelector(
+    "#restrictions-page__restricted-ingredients-form"
+);
 
-// set it to localStorage
+//sets the value of variable restrictedIngredients to that of user input within the html of restrictions page
+let restrictedIngredients = document.querySelector(`input[name="restrictedIngredients"]`);
 
+//adds event listener to restrictions page for excluded ingredients
+ingredientsFormRestrictionsPage.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    //Split the input value into an array of ingredients
+    let formData = restrictedIngredients.value.split(" ");
+    //join the array as a comma seperated list
+    let finalizedData = formData.length > 1 ? formData.join(",") : formData[0]
+    //save the items to local storage
+    localStorage.setItem("excludedItems", finalizedData);
+});
+
+// not sure how the page for restricted diets is going to be set up yet but the local storage variable should be named dietaryRestrictions and should be assigned a value later
+localStorage.setItem("dietaryRestrictions", value)

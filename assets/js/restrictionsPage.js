04fdde6a653062a47cf7783e084a8b6cf892bcd6
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-04-19 14:44:31 -0600

    added the styling to the restrictions page and the functionality to add restrictions

--- assets/js/restrictionsPage.js
+++ assets/js/restrictionsPage.js
@@ -1,29 +1,71 @@
 // get user restrictions info
 //sets the ingredientsFormRestrictionsPage variable to a form from the restirctions page html
 let options = JSON.parse(localStorage.getItem("options"));
-let fakeRestrictionsData = {
-  // ...options,
-  excludedItems: ["onions", "chicken"],
-  // dietaryRestrictions: "vegetarian",
+let currentDietTag = document.querySelector(".current-diet");
+const dietToId = (diet) => {
+  console.log(diet);
+  if (diet.split("-")) {
+    let dietToId = diet.split("-");
+    if (dietToId[1]) {
+      dietToId[1] = `${dietToId[1][0].toUpperCase()}${dietToId[1].substring(
+        1
+      )}`;
+    }
+
+    dietToId = dietToId.join("").trim(" ");
+    return dietToId;
+  }
+  return diet;
 };
-console.log({ fakeRestrictionsData });
-localStorage.setItem("options", JSON.stringify(fakeRestrictionsData));
-let ingredientsFormRestrictionsPage = document.querySelector(
-  "#restrictions-page__restricted-ingredients-form"
-);
-//sets the value of variable restrictedIngredients to that of user input within the html of restrictions page
-let restrictedIngredients = document.querySelector(
-  `input[name="restrictedIngredients"]`
-);
-//adds event listener to restrictions page for excluded ingredients
-ingredientsFormRestrictionsPage.addEventListener("submit", (e) => {
-  e.preventDefault();
-  //Split the input value into an array of ingredients
-  let formData = restrictedIngredients.value.split(" ");
-  //join the array as a comma seperated list
-  let finalizedData = formData.length > 1 ? formData.join(",") : formData[0];
-  //save the items to local storage
-  localStorage.setItem("excludedItems", finalizedData);
+if (options.dietaryRestrictions) {
+  let id = dietToId(options.dietaryRestrictions);
+  console.log(id);
+  let currentDiet = document.querySelector(`#${id}`);
+  console.log({ currentDiet });
+
+  currentDietTag.textContent = options.dietaryRestrictions;
+  currentDiet.checked = true;
+}
+// let fakeRestrictionsData = {
+//   // ...options,
+//   excludedItems: ["onions", "chicken"],
+//   // dietaryRestrictions: "vegetarian",
+// };
+// console.log({ fakeRestrictionsData });
+// localStorage.setItem("options", JSON.stringify(fakeRestrictionsData));
+// let ingredientsFormRestrictionsPage = document.querySelector(
+//   "#restrictions-page__restricted-ingredients-form"
+// );
+// //sets the value of variable restrictedIngredients to that of user input within the html of restrictions page
+// let restrictedIngredients = document.querySelector(
+//   `input[name="restrictedIngredients"]`
+// );
+// //adds event listener to restrictions page for excluded ingredients
+// ingredientsFormRestrictionsPage.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   //Split the input value into an array of ingredients
+//   let formData = restrictedIngredients.value.split(" ");
+//   //join the array as a comma seperated list
+//   let finalizedData = formData.length > 1 ? formData.join(",") : formData[0];
+//   //save the items to local storage
+//   localStorage.setItem("excludedItems", finalizedData);
+// });
+// // not sure how the page for restricted diets is going to be set up yet but the local storage variable should be named dietaryRestrictions and should be assigned a value later
+// localStorage.setItem("dietaryRestrictions", value);
+let restrictionElements = Array.from(document.querySelectorAll(".diet"));
+
+restrictionElements.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    console.log("CLICKED");
+    if (e.target.checked) {
+      currentDietTag.textContent = e.target.value;
+      let optionsWithRestrictions = {
+        ...options,
+        dietaryRestrictions: e.target.value,
+      };
+
+      localStorage.setItem("options", JSON.stringify(optionsWithRestrictions));
+    }
+  });
 });
-// not sure how the page for restricted diets is going to be set up yet but the local storage variable should be named dietaryRestrictions and should be assigned a value later
-localStorage.setItem("dietaryRestrictions", value);
+console.log(restrictionElements);
--- helpers/mealsCard.js
+++ helpers/mealsCard.js
@@ -5,23 +5,27 @@ export function mealsCard(meal) {
   const parser = new DOMParser();
   let item = parser.parseFromString(
     `
-    <div  id=${meal.id
-    } class="results-item relative  flex bg-whiteFull rounded-[10px] max-w-[890px] h-[230px] overflow-hidden shadow-med">
+    <div  id=${
+      meal.id
+    } class="results-item relative overflow-hidden  flex bg-whiteFull  rounded-[10px] max-w-[890px]   shadow-med">
     <div class="opacity-0 bg-[rgba(0, 0, 0, 0.656)] result-backdrop  absolute w-full h-full z-[1000]">
     </div>
     <div class="result-actions w-full h-full absolute z-[2000]">
     <div id=${meal.id} class="absolute save-btn opacity-0">
-          ${meal?.isSaved
-      ? `saved`
-      : `<img id=${meal.id} src="../assets/svg/bookmark-not-saved.svg"/>`
-    }
+          ${
+            meal?.isSaved
+              ? `saved`
+              : `<img id=${meal.id} src="../assets/svg/bookmark-not-saved.svg"/>`
+          }
           </div>
-    <a class="absolute opacity-0" href=${meal.spoonacularSourceUrl
+    <a class="absolute opacity-0" href=${
+      meal.spoonacularSourceUrl
     } target="_blank "><img src="../assets/svg/link.svg"/></a>
     </div>
         <div class="flex">
-        <div href=${meal.spoonacularSourceUrl
-    } target="_blank" class="results-img-container">
+        <div href=${
+          meal.spoonacularSourceUrl
+        } target="_blank" class="results-img-container">
           <img
             class=" results-img  max-w-[224px] h-full object-cover"
             src="${meal.image}"
@@ -33,44 +37,50 @@ export function mealsCard(meal) {
             <h4 class="text-[14px] mb-[6px]">${meal.title}</h4>
             <div class="resultItem-tags flex gap-[8px] items-center">
             ${meal.diets
-      .map((diet, i) => {
-        let item;
-        let bgColor = diet.split(" ");
-        if (bgColor.length > 1) {
-          bgColor = `${bgColor[0]
-            }${bgColor[1][0].toUpperCase()}${bgColor[1].substring(
-              1,
-              bgColor[0].length
-            )}`;
-        }
-        console.log(meal.id);
-        item = `<span  class="block ${i >= 3 ? "hidden" : ""
-          } bg-${bgColor} text-[8px] rounded-full px-[10px] py-[5px]">
+              .map((diet, i) => {
+                let item;
+                let bgColor = diet.split(" ");
+                if (bgColor.length > 1) {
+                  bgColor = `${
+                    bgColor[0]
+                  }${bgColor[1][0].toUpperCase()}${bgColor[1].substring(
+                    1,
+                    bgColor[0].length
+                  )}`;
+                }
+                console.log(meal.id);
+                item = `<span  class="block ${
+                  i >= 3 ? "hidden" : ""
+                } bg-${bgColor} text-[8px] rounded-full px-[10px] py-[5px]">
                 ${diet}
               </span>`;
-        return item;
-      })
-      .join(" ")}
-              ${meal.cheap
-      ? `<span class="block text-[8px] bg-cheap rounded-full px-[13px] py-[5px]">
+                return item;
+              })
+              .join(" ")}
+              ${
+                meal.cheap
+                  ? `<span class="block text-[8px] bg-cheap rounded-full px-[13px] py-[5px]">
                 Cheap
               </span>`
-      : ""
-    }
-              ${meal.veryPopular
-      ? `<span class="block text-[8px] bg-veryPopular rounded-full px-[13px] py-[5px]">
+                  : ""
+              }
+              ${
+                meal.veryPopular
+                  ? `<span class="block text-[8px] bg-veryPopular rounded-full px-[13px] py-[5px]">
                 Very popular
               </span>`
-      : ""
-    }
-              <span class="text-[8px] ${meal.diets.length < 3 ? "hidden" : "block"
-    }">
+                  : ""
+              }
+              <span class="text-[8px] ${
+                meal.diets.length < 3 ? "hidden" : "block"
+              }">
                 ${meal.diets.length > 3 ? `${meal.diets.length - 3}+` : ""}
               </span>
             </div>
           </div>
           <p class="text-[12px] leading-[19.5px]">
-           ${meal.summary.substring(0, 500)}${meal.summary.length >= 300 && "..."
+           ${meal.summary.substring(0, 600)}${
+      meal.summary.length >= 300 && "..."
     }
           </p>
         </div>
